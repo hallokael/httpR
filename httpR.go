1cf48e24598383e0d5d7311eb6d2c3dcc6045445
liuziyao <liuziyao@bytedance.com>
2019-03-21 18:35:32 +0800

    fix

--- httpR.go
+++ httpR.go
@@ -11,24 +11,26 @@ type HttpRun struct {
 	FileName string
 	Code     int
 	Params   []string
+	Resp     interface{}
 }
 
-func StatusOK(params ...string) HttpRun {
-	return CommonRequest(http.StatusOK, params...)
+func StatusOK(resp interface{}, params ...string) HttpRun {
+	return CommonRequest(http.StatusOK, resp, params...)
 }
-func StatusInternalServerError(params ...string) HttpRun {
-	return CommonRequest(http.StatusInternalServerError, params...)
+func StatusInternalServerError(resp interface{}, params ...string) HttpRun {
+	return CommonRequest(http.StatusInternalServerError, resp, params...)
 }
-func StatusBadRequest(params ...string) HttpRun {
-	return CommonRequest(http.StatusBadRequest, params...)
+func StatusBadRequest(resp interface{}, params ...string) HttpRun {
+	return CommonRequest(http.StatusBadRequest, resp, params...)
 }
-func CommonRequest(code int, param ...string) HttpRun {
+func CommonRequest(code int, resp interface{}, param ...string) HttpRun {
 	line, fileName := GetCallerMessage()
 	return HttpRun{
 		Line:     line,
 		FileName: fileName,
 		Code:     code,
 		Params:   param,
+		Resp:     resp,
 	}
 }
 func GetCallerMessage() (int, string) {
